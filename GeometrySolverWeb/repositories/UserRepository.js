5c51e7501696ebe7066d2689161b3da7c17bf98b
Mladen <m.antic@elfak.rs>
2025-02-04 18:35:55 +0100

    bug fixes

--- GeometrySolverWeb/repositories/UserRepository.js
+++ GeometrySolverWeb/repositories/UserRepository.js
@@ -3,37 +3,49 @@ const UserModel = require('./UserModel.js');
 class UserRepository {
   constructor() {}
 
-  async createUser(userData) {
-    try {
+  async createUser(userData)
+  {
+    try
+    {
       const user = await UserModel.create(userData);
       return user;
-    } catch (error) {
+    }
+    catch(error)
+    {
       throw error;
     }
   }
 
-  async getUserByUsername(username) {
-    try {
+  async getUserByUsername(username)
+  {
+    try
+    {
       const user = await UserModel.findOne(username);
       return user;
-    } catch (error) {
+    }
+    catch (error)
+    {
       throw error;
     }
   }
 
-  async getUserById(id) {
-    try {
+  async getUserById(id)
+  {
+    try
+    {
       const user = await UserModel.findById(id);
       return user;
-    } catch (error) {
+    }
+    catch (error)
+    {
       throw error;
     }
   }
 
-  //add to/update projects list
-  async addProject(id, bodyID) {
-    try {
-
+  async addProject(id, bodyID)
+  {
+    try
+    {
       const filter = {_id: id};
       const update = { $push: { myProjects: bodyID } };
       const result = await UserModel.updateOne(filter, update);
@@ -47,28 +59,31 @@ class UserRepository {
         console.log("nothing happened...");
       }
 
-    } catch (error) {
+    }
+    catch (error)
+    {
       throw error;
     }
   }
   
-  async removeProject(id, projectID) {
-    try {
-      const result = await UserModel.findByIdAndUpdate(
-        id,
-        { $pull: { myProjects: { projectid: projectID } } }
-      );
+  async removeProject(id, projectID)
+  {
+    try
+    {
+      const result = await UserModel.findByIdAndUpdate(id, {$pull: {myProjects: {projectid:projectID}}});
   
-      if (!result) {
+      if (!result)
+      {
         throw new Error('User not found');
       }
   
       return { success: true, message: 'Project removed from user projects' };
-    } catch (error) {
+    }
+    catch (error)
+    {
       throw error;
     }
   }
-
 }
 
 module.exports = new UserRepository();
